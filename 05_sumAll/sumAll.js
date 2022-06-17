376bb8b7deb29d919d72d7678ea708056c3a0808
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-06-16 19:23:57 -0500

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+  const numbers = [...arguments].sort();
 
+  if (!numbers.every((n) => typeof n === 'number' && n > -1)) {
+    return 'ERROR';
+  }
+
+  let sum = 0;
+  for (let addend = numbers[0]; addend <= numbers[1]; addend++) {
+    sum += addend;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
