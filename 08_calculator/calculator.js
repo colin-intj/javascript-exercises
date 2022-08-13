2f091cbabb4c61ff2b80d3746228d320b7a99acf
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-08-13 16:43:30 -0500

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,19 @@
-const add = function() {
-	
-};
+const add = (addend1 = 0, addend2 = 0) => addend1 + addend2;
 
-const subtract = function() {
-	
-};
+const subtract = (minuend = 0, subtrahend = 0) => minuend - subtrahend;
 
-const sum = function() {
-	
-};
+const sum = (numbers = []) => numbers.reduce((a, b) => a + b, 0);
 
-const multiply = function() {
+const multiply = (numbers = []) => numbers.reduce((a, b) => a * b, 1);
 
-};
+const power = (base = 1, exponent = 1) => base ** exponent;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (number = 0) => {
+  let fact = 1;
+  for (let i = number - 1; i > -1; i--) {
+    fact *= (number - i);
+  }
+  return fact;
 };
 
 // Do not edit below this line
