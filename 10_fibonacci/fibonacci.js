d2def6689db978bcb957b53cfc5496a69cd688c8
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-08-13 19:15:29 -0500

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
-const fibonacci = function() {
-
+const fibonacci = (n) => {
+  if (n < 0) return 'OOPS';
+  return ~~(((1 + 5 ** .5) ** n - (1 - 5 ** .5) ** n) / (2 ** n * 5 ** .5));
 };
 
 // Do not edit below this line
