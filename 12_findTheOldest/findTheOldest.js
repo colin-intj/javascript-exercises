7f72a9f103583e2f5dca93c252859d09f9f089ba
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-09-04 18:36:34 -0500

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = (people = []) => {
+ const currentYear = new Date().getFullYear();
 
+ for (const person of people) {
+    /*
+     * Subtracts the year of birth from the the year of death. If the object
+     * doesn't have a year of death, then the current year will be used instead.
+     */
+    person.yearsLived = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+  }
+
+  const oldestAge = Math.max(...people.map((n) => n.yearsLived));
+
+  return people.find((n) => n.yearsLived === oldestAge);
 };
 
 // Do not edit below this line
